8cc9ec72a14fb182f4c96624ad656a8bf94af896
nikgeh <nikgeh@gmail.com>
2011-08-02 21:34:37 -0700

    Crated blank PlacePhotosTableViewController

---
+++ Places/PlacePhotosTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  PlacePhotosTableViewController.h
+//  Places
+//
+//  Created by NICHOLAS GEH on 8/2/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlacePhotosTableViewController : UITableViewController
+
+@end
